434194904a1d31fd15a2d5dc921e9d080f5d6c83
Kevin Black <kevin3.black@gmail.com>
2023-06-26 13:44:46 -0700

    Switch carousel to slick

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,39 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+$(document).ready(function () {
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
 
-    });
+  });
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
-    }
+  // carousel initialization
+  const options = {
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    loop: true,
+    infinite: true,
+    autoplay: false,
+    dots: true,
+    draggable: false,
+    lazyLoad: 'progressive',
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  }
 
-		// Initialize all div with carousel class
-    bulmaCarousel.attach('.carousel', options);
+  $('.carousel').slick(options);
 })
